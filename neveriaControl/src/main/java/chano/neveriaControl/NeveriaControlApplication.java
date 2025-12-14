4d26d294602ac68ca3301ab5643b0dc2482696d3
ParkerJavi <parkerjavi@gmail.com>
2025-12-14 17:56:19 -0600

    Genere el archivo correspondiente a spring boot

---
+++ neveriaControl/src/main/java/chano/neveriaControl/NeveriaControlApplication.java
@@ -0,0 +1,13 @@
+package chano.neveriaControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeveriaControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeveriaControlApplication.class, args);
+	}
+
+}
---
+++ neveriaControl/src/test/java/chano/neveriaControl/NeveriaControlApplicationTests.java
@@ -0,0 +1,13 @@
+package chano.neveriaControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeveriaControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
